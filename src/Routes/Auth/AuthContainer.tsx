207dffc27067462619087fe5738d1fde27310c8a
yoonwonyoul <yoonwonyoul@webchemist.net>
2019-06-13 20:36:53 +0900

    feat: add MVP pattern

---
+++ src/Routes/Auth/AuthContainer.tsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+const AuthContainer = () => {
+  return <div>aa</div>
+}
+
+export default AuthContainer
---
+++ src/Routes/Auth/AuthPresenter.tsx
@@ -0,0 +1,8 @@
+import React from "react"
+import AuthContainer from "./AuthContainer";
+
+const AuthPresenter = () => {
+  return <AuthContainer />
+}
+
+export default AuthPresenter;
